16253b5fcd3eef69188090e6533849a107138317
msaadirfan <msaadirfan04@gmail.com>
2026-08-02 05:29:55 +0500

    Add JWT authentication and authorization endpoints along with sessions

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import userModel from '../models/user.model.js';
+import sessionModel from '../models/session.model.js';
 import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 
@@ -27,17 +28,27 @@ export const register = async(req, res)=>{
         password : hashedPassword
     })
 
-    const accessToken = jwt.sign({
-        id: user._id,
-    }, process.env.JWT_SECRET,{
-        expiresIn: "15m"
-    } 
-    )
+    
     
     const refreshToken = jwt.sign({
         id: user._id,
     }, process.env.JWT_SECRET,{
         expiresIn: "7d"
+    } 
+    )
+
+    const session = await sessionModel.create({
+        user: user._id,
+        refreshTokenHash: crypto.createHash("sha256").update(refreshToken).digest("hex"),
+        ip: req.ip,
+        userAgent: req.headers['user-agent']
+    })
+
+    const accessToken = jwt.sign({
+        id: user._id,
+        sessionId: session._id
+    }, process.env.JWT_SECRET,{
+        expiresIn: "15m"
     } 
     )
 
@@ -103,6 +114,19 @@ export const refreshToken = async(req, res)=>{
             refreshToken, process.env.JWT_SECRET
         );
 
+        const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+
+        const session = await sessionModel.findOne({
+            refreshTokenHash,
+            revoked: false
+        });
+
+        if(!session){
+            return res.status('401').json({
+                message: "Invalid refresh token"
+            })
+        }
+
         const accessToken = jwt.sign({
             id: decoded.id,
         }, process.env.JWT_SECRET,{
@@ -115,6 +139,13 @@ export const refreshToken = async(req, res)=>{
             expiresIn: "7d"
         } 
         );
+
+        const newRefreshTokenHash = crypto.createHash("sha256").update(newRefreshToken).digest("hex");
+
+        session.refreshTokenHash = newRefreshTokenHash;
+        await session.save();
+
+        
 
         res.cookie("refreshToken", newRefreshToken, {
             httpOnly: true,
@@ -129,6 +160,121 @@ export const refreshToken = async(req, res)=>{
         });
 }
 
+export const logout = async(req, res)=>{
+
+    const refreshToken = req.cookies.refreshToken;
+
+    if(!refreshToken){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+
+    const session = await sessionModel.findOne({
+        refreshTokenHash: crypto.createHash("sha256").update(refreshToken).digest("hex"),
+        revoked: false
+    });
+
+    if(!session){
+        return res.status(401).json({
+            message: "Invalid refresh token"
+        })
+    }
+
+    session.revoked = true;
+    await session.save();
+
+    res.clearCookie("refreshToken");
+    res.status(200).json({
+        message: "Logged out successfully"
+    });
+}
 
 
+export const logoutAll = async(req, res) =>{
+
+    const refreshToken = req.cookies.refreshToken;
+
+    if(!refreshToken){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    const decoded = jwt.verify(refreshToken, process.env.JWT_SECRET);
+    
+    const session = await sessionModel.updateMany({
+        user: decoded.id,
+        revoked: false
+    }, {
+        revoked: true
+    });
+
+    if(session.matchedCount == 0){
+        return res.status(400).json({
+            message: "No active sessions found"
+        })
+    }
+
+    res.clearCookie("refreshToken");
+
+    res.status(200).json({
+        message: "Logged out of all devices successfully"
+    })
+}
+
+export const login = async(req, res)=>{
+    const {email, password}=req.body;
+    
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.findOne({
+       email: email,
+       password:hashedPassword
+        
+    });
+
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    
+    const refreshToken = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET,{
+        expiresIn: "7d"
+    } 
+    )
+
+    const session = await sessionModel.create({
+        user: user._id,
+        refreshTokenHash: crypto.createHash("sha256").update(refreshToken).digest("hex"),
+        ip: req.ip,
+        userAgent: req.headers['user-agent']
+    })
+
+    const accessToken = jwt.sign({
+        id: user._id,
+        sessionId: session._id
+    }, process.env.JWT_SECRET,{
+        expiresIn: "15m"
+    } 
+    )
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000 //7 days
+    })
+
+    res.status(200).json({
+        message: "Logged in successfully",
+        username: user.username,
+        accessToken
+    })
+}
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,8 +4,15 @@ const authRouter = Router();
 
 authRouter.post("/register", authController.register);
 
-authRouter.post("/get-me", authController.getMe);
+authRouter.post("/login", authController.login);
+
+authRouter.get("/me", authController.getMe);
 
 authRouter.post("/refresh-token", authController.refreshToken);
+
+authRouter.post("/logout", authController.logout);
+
+authRouter.post("/logout-all", authController.logoutAll);
+
 
 export default authRouter;
