671c143115d847420132eb91dec3f5e912e85df2
msaadirfan <msaadirfan04@gmail.com>
2026-08-09 11:25:14 +0500

    Resolve CodeRabbit comments

--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,6 +1,8 @@
 import logger from "../utils/logger.js";
-const errorMiddleware = (err, req, res, next)=>{
+
+const errorMiddleware = (err, req, res, next) => {
     const status = err.statusCode ?? err.status ?? 500;
+
     const message = status >= 500
         ? "Internal server error"
         : err.message || "Bad Request";
@@ -10,13 +12,18 @@ const errorMiddleware = (err, req, res, next)=>{
             err,
             status: status,
             message: message
-        }, "Request Failed"
-        );
+        },
+        "Request Failed"
+    );
+
+    if (res.headersSent) {
+        return next(err);
+    }
 
     return res.status(status).json({
         success: "fail",
         message: message
     });
-}
+};
 
 export default errorMiddleware;
